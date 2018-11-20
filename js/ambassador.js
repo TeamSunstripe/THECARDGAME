9c34317621277a0f40b4466597875b822b37248e
YouOhshima <info@sunstripe.main.jp>
2018-11-20 19:57:42 +0900

    アンバサダー紹介ページ #53 - ambassador - js / ambassador.js

---
+++ js/ambassador.js
@@ -0,0 +1,355 @@
+var ambassadorList = [
+                      /* にしかわちなみ */
+                      // にしかわちなみ
+                      {
+                      title :"にしかわちなみ",
+                      description:"がおぉ〜",
+                      img:"../images/card/chinamiishikawa.png",
+                      href:"../card/chinaminishikawa.html",
+                      },
+                      // にしかわちなみ
+                      {
+                      title :"にしかわちなみ",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/chinamiishikawa_roid.png",
+                      href:"../card/chinaminishikawa.html",
+                      hidden:true,
+                      },
+                      // にしかわちなみ
+                      {
+                      title :"にしかわちなみ",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/chinamiishikawa_example.png",
+                      href:"../card/chinaminishikawa.html",
+                      hidden:true,
+                      },
+                      
+                      /* 釘町みやび */
+                      {
+                      title :"釘町みやび",
+                      description:"つくねを食べて元気になろう！",
+                      img:"../images/card/kugimachimiyabi.png",
+                      href:"../card/kugimachimiyabi.html",
+                      new:true,
+                      },
+                      {
+                      title :"釘町みやび",
+                      description:"食いしん坊アイドル 釘町みやび",
+                      img:"../images/card/kugimachimiyabi_twitter.png",
+                      href:"../card/kugimachimiyabi.html",
+                      new:true,
+                      },
+                      {
+                      title :"釘町みやび",
+                      description:"もりもり食べて元気になろう！",
+                      img:"../images/card/kugimachimiyabi_eo1l20.png",
+                      href:"../card/kugimachimiyabi.html",
+                      new:true,
+                      },
+
+                      /* こまいけんと */
+                      // こまいけんと
+                      {
+                      title :"こまいけんと",
+                      description:"あさがやドラムさんにてアニアコやアコギな夜に共演させていただいてます！",
+                      img:"../images/card/kentokomai_eo1l20.png",
+                      href:"../card/kentokomai.html",
+                      new:true,
+                      },
+                      // こまいけんと
+                      {
+                      title :"こまいけんと",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      new:true,
+                      },
+                      
+                      /* 伊藤麻衣 */
+                      {
+                      title :"伊藤麻衣",
+                      description:"おはきょろございます！！",
+                      img:"../images/card/kyoro.png",
+                      href:"../card/kyoro.html",
+                      },
+                      {
+                      title :"伊藤麻衣",
+                      description:"おはきょろございます！！",
+                      img:"../images/card/kyoro_twitter.png",
+                      href:"../card/kyoro.html",
+                      },
+                      
+                      /* 岡本美緒 */
+                      {
+                      title :"岡本美緒",
+                      description:"ちゃんみお王国民にみんななろうっ♡",
+                      img:"../images/card/miookamoto.png",
+                      href:"../card/miookamoto.html",
+                      },
+                      
+                      {
+                      title :"岡本美緒",
+                      description:"ちゃんみお王国民にみんななろうっ♡",
+                      img:"../images/card/miookamoto_showroom.png",
+                      href:"../card/miookamoto.html",
+                      },
+                      
+                      {
+                      title :"岡本美緒",
+                      description:"岡本美緒＠ちゃんみお",
+                      img:"../images/card/miookamoto_twitter.png",
+                      href:"../card/miookamoto.html",
+                      },
+                      
+                      /* ぴんきー */
+                      {
+                      title :"ぴんきー",
+                      description:"ぴんきーぴんくるーむ★",
+                      img:"../images/card/pinky.png",
+                      href:"../card/pinky.html",
+                      },
+                      // BB事務所
+                      
+                      /*
+                       {
+                       title :"BB事務所",
+                       description:"THE☆へんテごんずもよろしくお願いいたします。",
+                       img:"../images/card/kentokomai.png",
+                       href:"../card/kentokomai.html",
+                       },
+                       */
+
+                      /* 古澤菜奈美 */
+                      {
+                      title :"古澤菜奈美",
+                      description:"なーみのほしぐみさんルーム",
+                      img:"../images/card/nanamifurusawa.png",
+                      href:"../card/nanamifurusawa.html",
+                      },
+                      
+                      /* 青桐亜子 */
+                      {
+                      title :"青桐亜子",
+                      description:"あこ語りたいっす！",
+                      img:"../images/card/aogiriako.png",
+                      href:"../card/aogiriako.html",
+                      },
+
+                      /*
+                      {
+                      title :"I LOVE 下北沢",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"THE Media",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"アクションカード",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"アクロスエンターテイメント",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      
+                      /*
+                      {
+                      title :"アサインプロモーション",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"あらたまなつこ",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"おとまむ系ロディー",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"きゃらあげ素敵曜日チャンネル",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"クルーズ",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"グルメ",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"トレンド",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      {
+                      title :"はるなあはる",
+                      description:"沖縄生まれ沖縄在住のはるなあはるです。",
+                      img:"../images/card/harunaaharu.png",
+                      href:"../card/harunaaharu.html",
+                      },
+                      {
+                      title :"ほたるんひめ",
+                      description:"非営利活動個人",
+                      img:"../images/card/hotarunhime.png",
+                      href:"../card/hotarunhime.html",
+                      },
+                      /*
+                      {
+                      title :"ライストゥーミートユー",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"ルイーズ",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"会場",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                      */
+                      
+                      /* 装美スタジオ */
+                      {
+                      title :"装美スタジオ",
+                      description:"そうびスタジオ",
+                      img:"../images/card/soubistudio.png",
+                      href:"../card/soubistudio.html",
+                      },
+                      
+                      /* 木星劇場 */
+                      {
+                      title :"木星劇場",
+                      description:"もくせいげきじょう",
+                      img:"../images/card/mokuseigekijo.png",
+                      href:"../card/mokuseigekijo.html",
+                      },
+                      
+                      /*
+                      {
+                      title :"叶枝心",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"渋谷原宿女子会議",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      {
+                      title :"深沢ばにら",
+                      description:"深沢ばにらのばに☆らぼ",
+                      img:"../images/card/vanillafukazawa.png",
+                      href:"../card/vanillafukazawa.html",
+                      },
+                      {
+                      title :"相沢凛",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      hidden:true,
+                      },
+                      /*
+                      {
+                      title :"妖エンターテインメント",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"内田麻友",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/kentokomai_twitter.png",
+                      href:"../card/kentokomai.html",
+                      },
+                       */
+                      /*
+                      {
+                      title :"NO IMAGE",
+                      description:"ノーイメージ",
+                      img:"../images/card/no-image.png.png",
+                      href:"../card/card.html",
+                      },
+                       */
+                      {
+                      title :"プロデュースカード",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/action/produce.png",
+                      href:"../card/action/producecard.html",
+                      },
+                      {
+                      title :"QRカード",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/action/qrcode.png",
+                      href:"../card/action/qrcode.html",
+                      },
+                      
+                      /*
+                       */
+                      {
+                      title :"カリキュラム",
+                      description:"THE☆へんテごんずもよろしくお願いいたします。",
+                      img:"../images/card/word/curriculum.png",
+                      href:"../card/word/curriculum.html",
+                      },
+
+];
