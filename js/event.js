c1fe30edec025ac902212776ca68d2c9c094afe7
YouOhshima <info@sunstripe.main.jp>
2018-11-25 17:34:40 +0900

    EventList #70

---
+++ js/event.js
@@ -0,0 +1,269 @@
+var eventList_div = document.getElementById("eventList");
+//alert(eventList_div);
+
+var eventList = {
+    "event2018":[
+                 //event201812060001
+                 {
+                 id:"event201812060001",
+                 date:"12月06日（木）",
+                 title:"素敵曜日！ナイトスクーる。（13）公開生放送",
+                 cast:"釘町みやび,伊藤麻衣,岡本美緒,ぴんきー",
+                 image:"http://night-school.karaage.site/img/main_image.png",
+                 time:"22:00-22:40",
+                 timelimit:"2019年12月31日 00:00",
+                 maxtime:"00:37:00",
+                 ticketInfo:"開演18:30通常入場券0/50枚<br />開演18:30推しカード付き0/50枚",
+                 stopURL:"",
+                 reserveURL:"https://tiget.net/users/1845",
+                 
+                 // 投稿者オーナー
+                 owner:{
+                 title:"THE CARD GAME",
+                 image:"http://stage-card.karaage.site/images/thecardgame_icon.png",
+                 profile:"「声優・タレント・アーティスト」アンバサダーをサポートする<br />「THE CARD GAME」とは？",
+                 url:"http://stage-card.karaage.site/card.html",
+                 },
+                 
+                 site:{
+                 title:"イベントサイト",
+                 url:"http://night-school.karaage.site",
+                 },
+                 
+                 },
+                 //event201812130001
+                 {
+                 id:"event201812130001",
+                 date:"12月13日（木）",
+                 title:"４米：ライストゥーミートユー！！公開生放送",
+                 cast:"角川未歩,山本幸奈,きゃらあげP",
+                 image:"https://pbs.twimg.com/profile_images/916453194862956544/cH2QhS_s_400x400.jpg",
+                 time:"22:20-22:40",
+                 timelimit:"2019年12月31日 00:00",
+                 maxtime:"00:17:00",
+                 ticketInfo:"開演18:300/50枚",
+                 stopURL:"",
+                 reserveURL:"https://tiget.net/users/1845",
+                 
+                 // 投稿者オーナー
+                 owner:{
+                 title:"素敵曜日！ナイトスクーる",
+                 image:"https://s3-ap-northeast-1.amazonaws.com/plus.tiget.net/uploads/users/239/2aa205fd-5773-4c5b-86d0-f5f29c5a61d4.png",
+                 profile:"素敵曜日！ナイトスクーる | メディア情報学部",
+                 url:"http://night-school.karaage.site",
+                 },
+                 
+                 site:{
+                 title:"イベントサイト",
+                 url:"http://welkome.sunstripe-main.jp/e",
+                 },
+                 
+                 },
+                 //event201812200001
+                 {
+                 id:"event201812200001",
+                 date:"12月20日（木）",
+                 title:"素敵曜日！ナイトスクーる。（15）公開生放送",
+                 cast:"こまいけんと,伊藤麻衣,岡本美緒,ぴんきー",
+                 image:"http://night-school.karaage.site/img/main_image.png",
+                 time:"22:20-22:40",
+                 timelimit:"2019年12月31日 00:00",
+                 maxtime:"00:17:00",
+                 ticketInfo:"開演18:30通常入場券0/50枚<br />開演18:30推しカード付き0/50枚",
+                 stopURL:"",
+                 reserveURL:"https://tiget.net/users/1845",
+                 
+                 // 投稿者オーナー
+                 owner:{
+                 title:"THE CARD GAME",
+                 image:"http://stage-card.karaage.site/images/thecardgame_icon.png",
+                 profile:"「声優・タレント・アーティスト」アンバサダーをサポートする<br />「THE CARD GAME」とは？",
+                 url:"http://stage-card.karaage.site/card.html",
+                 },
+                 
+                 site:{
+                 title:"イベントサイト",
+                 url:"http://night-school.karaage.site",
+                 },
+                 
+                 },
+                 //event201812210001
+                 {
+                 id:"event201812210001",
+                 date:"12月21日（金）",
+                 title:"「素敵曜日！ナイトスクーる。」クリスマス♪第１回出張イベント（限定公開）",
+                 cast:"釘町みやび,こまいけんと,伊藤麻衣,岡本美緒,ぴんきー",
+                 image:"http://night-school.karaage.site/img/main_image.png",
+                 time:"18:00-20:30",
+                 timelimit:"2019年12月31日 00:00",
+                 maxtime:"02:00:00",
+                 ticketInfo:"開演18:30通常入場券0/50枚<br />開演18:30推しカード付き0/50枚",
+                 stopURL:"",
+                 reserveURL:"https://tiget.net/users/1845",
+                 
+                 // 投稿者オーナー
+                 owner:{
+                 title:"THE CARD GAME",
+                 image:"http://stage-card.karaage.site/images/thecardgame_icon.png",
+                 profile:"「声優・タレント・アーティスト」アンバサダーをサポートする<br />「THE CARD GAME」とは？",
+                 url:"http://stage-card.karaage.site/card.html",
+                 },
+                 
+                 site:{
+                 title:"イベントサイト",
+                 url:"http://night-school.karaage.site",
+                 },
+                 
+                 },
+                 //event201812270001
+                 {
+                 id:"event201812270001",
+                 date:"12月27日（木）",
+                 title:"素敵曜日！ナイトスクーる。（16）公開生放送",
+                 cast:"釘町みやび,こまいけんと,伊藤麻衣,岡本美緒,ぴんきー",
+                 image:"http://night-school.karaage.site/img/main_image.png",
+                 time:"22:00-22:40",
+                 timelimit:"2019年12月31日 00:00",
+                 maxtime:"00:17:00",
+                 ticketInfo:"開演18:30通常入場券0/50枚<br />開演18:30推しカード付き0/50枚",
+                 stopURL:"",
+                 reserveURL:"https://tiget.net/users/1845",
+                 
+                 // 投稿者オーナー
+                 owner:{
+                 title:"THE CARD GAME",
+                 image:"http://stage-card.karaage.site/images/thecardgame_icon.png",
+                 profile:"「声優・タレント・アーティスト」アンバサダーをサポートする<br />「THE CARD GAME」とは？",
+                 url:"http://stage-card.karaage.site/card.html",
+                 },
+                 
+                 site:{
+                 title:"イベントサイト",
+                 url:"http://night-school.karaage.site",
+                 },
+                 
+                 },
+                 ],
+    "event2019":[
+    
+    ],
+};
+
+function pageLink( url , target) {
+    
+    if (target) {
+        // 別のウィンドウ
+        open( url , target ) ;
+    }
+    else {
+        location.href = url;
+    }
+}
+
+function eventListInfo() {
+    var data = [];
+    var eventId = "event201811240001";
+    var eventListInfo = "";
+    
+    eventListInfo += '<div class="card_title">';
+    eventListInfo += '<h3 style="padding:20px;">イベント一覧</h3>';
+    eventListInfo += '</div>';
+    
+    // eventListInfo
+    for (eventYear in eventList) {
+        
+        eventListInfo += '<div id="' + eventYear + '">';
+        eventListInfo += '<h3 style="padding:20px;">';
+        eventListInfo += eventYear;
+        eventListInfo += '</h3>';
+        eventListInfo += '</div>';
+
+        for (var e = 0;e < eventList[eventYear].length;e++) {
+            // イベントID
+            eventListInfo += '<div id="';
+            eventListInfo += eventId;
+            eventListInfo += '" style="background-color:white;color:black;padding:10px;">';
+            // タイトル
+            eventListInfo += '<h1 style="background-color:rgba(100,100,200,0.5);color:black;padding:10px;font-size:20px">';
+            eventListInfo += eventList[eventYear][ e ].title;
+            eventListInfo += '</h1>';
+            eventListInfo += '<img style="float:left;width:30%;" src="';
+            eventListInfo += eventList[eventYear][ e ].image;
+            eventListInfo += '" title="' + eventList[eventYear][ e ].title + '" />';
+            // 詳細
+            eventListInfo += '<div style="float:right;width:66%;padding-left:2%;" >';
+            // 日時
+            eventListInfo += '<h3>';
+            eventListInfo += eventList[eventYear][ e ].date;
+            eventListInfo += ' ';
+            eventListInfo += eventList[eventYear][ e ].time;
+            eventListInfo += '</h3>';
+            eventListInfo += '<h3>キャスト：';
+            eventListInfo += eventList[eventYear][ e ].cast;
+            eventListInfo += '</h3>';
+            eventListInfo += '<h5 style="text-align:right;">掲載期間：';
+            eventListInfo += eventList[eventYear][ e ].timelimit;
+            eventListInfo += 'まで</h5>';
+            
+            eventListInfo += '<button style="width:30%;height:55px;" onclick=pageLink("';
+            eventListInfo += eventList[eventYear][ e ].reserveURL;
+            eventListInfo += '","_blank");>予約ページ</button>';
+            
+            eventListInfo += '</div>';
+            eventListInfo += '<hr style="clear:both;"/>';
+            // イベントサイト
+            eventListInfo += '<div>';
+            eventListInfo += '<h3 style="text-align:left;">';
+            eventListInfo += eventList[eventYear][ e ].site.title;
+            eventListInfo += '</h3>';
+            eventListInfo += '<a style="color:red;" href="';
+            eventListInfo += eventList[eventYear][ e ].site.url;
+            eventListInfo += '" target="_blank"/>';
+            eventListInfo += '<h3 style="text-align:left;">';
+            eventListInfo += eventList[eventYear][ e ].site.url;
+            eventListInfo += '</h3>';
+            eventListInfo += '</a>';
+            eventListInfo += '</div>';
+            
+            // 主催者・投稿者
+            eventListInfo += '<div>';
+            eventListInfo += '<h3 style="text-align:left;">';
+            eventListInfo += "主催者・投稿者";
+            eventListInfo += '</h3>';
+            eventListInfo += '<img style="width:55px;" src="';
+            eventListInfo += eventList[eventYear][ e ].owner.image;
+            eventListInfo += '" title="' + eventList[eventYear][ e ].id + '" />';
+
+            eventListInfo += '<h3 style="text-align:left;">';
+            eventListInfo += eventList[eventYear][ e ].owner.title;
+            eventListInfo += '</h3>';
+            eventListInfo += '<a style="color:red;" href="';
+            eventListInfo += eventList[eventYear][ e ].owner.url;
+            eventListInfo += '" target="_blank"/>';
+            eventListInfo += '<h3 style="text-align:left;">';
+            eventListInfo += eventList[eventYear][ e ].owner.url;
+            eventListInfo += '</h3>';
+            eventListInfo += '</a>';
+            eventListInfo += '<hr style="clear:both;"/>';
+            eventListInfo += '</div>';
+
+            
+            eventListInfo += '</div>';
+
+            eventListInfo += '<h6 style="text-align:right;padding:10px;">ID : ';
+            eventListInfo += eventId;
+            eventListInfo += '</h6>';
+
+        }
+    }
+    return eventListInfo;
+}
+
+eventList_div.innerHTML = eventListInfo();
+
+function recommendedEvent() {
+    var recommendedEventList = [];
+    return recommendedEventList;
+}
+
+document.write(recommendedEvent());
